bc242173d56ed26daaeaa3cf89cf02824036a326
Isa Tekinkaya <isatekinkaya@yahoo.de>
2021-06-23 19:42:16 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log('%c HI', 'color: firebrick')
+
+function getImageApi(){
+fetch('https://dog.ceo/api/breeds/image/random/4')
+.then(resp => resp.json())
+.then(json => { 
+  for( const imgUrl in json.message){
+  console.log(json.message[imgUrl])
+  const images = document.querySelector("#dog-image-container")
+    
+    images.innerHTML = images.innerHTML + `<img src=${json.message[imgUrl]}>`
+    
+    }
+  })
+}
+getImageApi()
